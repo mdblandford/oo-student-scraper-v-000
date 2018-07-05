a3cd0b400ebefd40d330bd214a65774400611293
Michael Blandford <mdblandford@gmail.com>
2018-07-05 16:28:26 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
 
     doc.css("div.student-card").each do |student|
       scraped_students << {
-        name: student.css("h4.student-name").text, 
+        name: student.css("h4.student-name").text,
         location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
     end
     scraped_students
