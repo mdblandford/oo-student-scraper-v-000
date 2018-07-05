8cfcbcfeafd5c818100a6f9404e3dfabf870610a
Michael Blandford <mdblandford@gmail.com>
2018-07-05 16:28:21 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = {}
+
+    doc.css("div.student-card").each do |student|
+      scraped_students << {
+        name: student.css("h4.student-name").text, 
+        location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
+    end
+    scraped_students
+   end
   end
 
   def self.scrape_profile_page(profile_url)
