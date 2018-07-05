94a1bbc63d9f92df56dfb3a1883606a7a440298c
Michael Blandford <mdblandford@gmail.com>
2018-07-05 16:34:49 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,6 +20,25 @@ class Scraper
    end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open(profile_url))
+    student = Hash.new
+ 	 
+    social_icons = doc.css("div.social-icon-container a").collect {|x| x.attribute("href").value}
+    social_icons.each do |social_icon|
+      if social_icon.include?("linkedin")
+        student[:linkedin] = social_icon
+      elsif social_icon.include?("github")
+        student[:github] = social_icon
+      elsif social_icon.include?("twitter")
+        student[:twitter] = social_icon
+      else
+        student[:blog] = social_icon
+      end
+    end
+      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
+      student[:profile_quote] = doc.css(".profile-quote").text
+
+    student
 
   end
 
