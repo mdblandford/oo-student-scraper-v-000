d74dcfc2819ca98d42f775f768de6ee9121ef0e2
Michael Blandford <mdblandford@gmail.com>
2018-07-05 16:32:02 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = Array.new
+
     doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
         name = student.css(".student-name").text
